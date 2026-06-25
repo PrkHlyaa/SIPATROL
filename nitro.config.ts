5cc4fd360eb931c33d8ff42c9c0b7be59faa9461
Zahra Hilyatul Jannah <150895570+PrkHlyaa@users.noreply.github.com>
2026-06-25 23:33:40 +0700

    fix bug deploy

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,4 +2,16 @@ import { defineNitroConfig } from "nitro/config";
 
 export default defineNitroConfig({
   preset: "vercel",
+  handlers: [
+    {
+      route: "/**",
+      handler: "./dist/server/server.js"
+    }
+  ],
+  publicAssets: [
+    {
+      baseURL: "/",
+      dir: "./dist/client"
+    }
+  ]
 });
